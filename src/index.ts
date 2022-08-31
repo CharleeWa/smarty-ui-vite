9fa9e6d016065dd74162200ecd52bd2236e9dc11
CharleeWa <18888351756@163.com>
2022-08-31 09:37:12 +0800

    feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+
+import SButton from "./button";
+import SFCButton from "./SFCButton.vue";
+import JSXButton from './JSXButton'
+// createApp(SButton)
+// createApp(SFCButton)
+
+createApp(JSXButton).mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+const rollupOptions = {
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+}
+
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  build: {
+    rollupOptions,
+    minify:false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
+      formats: ["es", "umd","iife"],
+    },
+  }
+})
